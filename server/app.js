d674649b91bf1e2857819fdf3ea857448cc412cb
Chitsanupong <first30952@gmail.com>
2021-09-29 11:15:15 +0700

    feat: add schema and enable graphql GUI

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
-// middleware - go to /graphql to open GraphQL GUI
-app.use('/graphql', graphqlHTTP({}));
+// middleware - go to /graphql to enable graphical tool
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
   console.log('server is listening on port 4000');
