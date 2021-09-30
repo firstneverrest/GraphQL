350cebe52ca792f0573166ffa19ee33188ba430d
Chitsanupong <first30952@gmail.com>
2021-09-30 10:50:41 +0700

    feat: add mongoose, env and connect to atlas

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: './.env' });
 
 const app = express();
+
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.1f312.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 // middleware - go to /graphql to enable graphical tool
 app.use(
